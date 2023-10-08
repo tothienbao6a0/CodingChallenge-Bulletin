37ea1d38bc37fee099e387c60757970a39323460
Bao To <tothienbao6a0@gmail.com>
2023-10-07 23:45:53 -0400

    push

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask
+from flask import Flask, render_template
 import pymongo
+from pymongo import MongoClient
 mongodb_uri = "mongodb+srv://tothienbao6a0:tintom@bulletinchallenge.5ltdrpd.mongodb.net/?retryWrites=true&w=majority"
 
 
@@ -15,5 +16,28 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello, World!'
 
+def index():
+    try:
+        # Connect to MongoDB
+        client = MongoClient(mongodb_uri)
+
+        # Access your database and collection
+        db = client['your-database-name']
+        collection = db['your-collection-name']
+
+        # Query the collection and retrieve data
+        data = collection.find()
+
+        # Render an HTML template with the data
+        return render_template('index.html', data=data)
+
+    except Exception as e:
+        return f"An error occurred: {str(e)}"
+
+    finally:
+        # Close the MongoDB connection
+        client.close()
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=80)
+
